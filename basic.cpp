7f46582ccf1f2397dd95b3dbad7ca7904bbcd791
Gopalkrushna sahu <sahugopalkrushna2@gmail.com>
2026-06-28 12:53:42 +0530

    learning basic of c++ and some conditional operator

--- basic.cpp
+++ basic.cpp
@@ -1,2 +1,52 @@
 
 
+#include<iostream>
+using namespace std;
+int main(){
+    // cout<<"hello"<<endl<<"world";
+
+    // cout << "hello" << "\n" << "worlddd";
+
+    // cout<<'4'<< endl;
+    // cout<< 4<< endl;
+    // cout<< 4+3<< endl;
+    // cout<<"4+3"<< endl;
+
+
+    // // UNDERSTANDING FLOAT
+    // float x = 4.5;
+    // float y = 6;
+    // cout << x << endl;
+    // cout << y << endl;
+
+
+    // // TAKING USER INPUT
+    // int x;
+    // cin >> x;
+    // cout << "input value is : " << x << endl;
+    // // cout << x << endl;
+
+
+    // // MODULUS
+    // cout<< (-3)%(-2);
+
+
+    // // TYPE CASTING
+    // int x;
+    // cin>>x;
+    // // float y = float(x);
+    // float y = (float)x;
+    // y = y/2;
+    // cout<<y<<endl;
+
+
+    // // PRINTING ASCII VALUE OF CHARACTER USING TYPE CASTING
+    // char ch = 'a';
+    // int x = int(ch);
+    // cout << x << endl;
+
+    // // CHAR TO INT
+    // char ch = 'A';
+    // int x = (int)ch;
+    // cout<< x << endl;
+}
---
+++ condition.cpp
@@ -0,0 +1,18 @@
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int x;
+    // cin>>x;
+    // // x>=0?cout<<x:cout<<-x;   //using ternary operator
+
+    // if(x>=0) cout<<x;   //using if-else
+    // else cout<<-x;
+
+
+
+
+}
