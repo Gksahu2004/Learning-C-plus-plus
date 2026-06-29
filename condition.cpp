0eb1fc6eddd7c1af7b5134291ba8a42cad097a40
Gopalkrushna sahu <sahugopalkrushna2@gmail.com>
2026-06-29 20:43:08 +0530

    Different types of conditional statements

--- condition.cpp
+++ condition.cpp
@@ -13,6 +13,126 @@ int main(){
     // else cout<<-x;
 
 
+    // // QUESTION 1: PROFIT OR LOSS
+    // // USING IF-ELSE
+
+    // // *Ques : If cost price and selling price of an item is
+    // // input through the keyboard, write a program to
+    // // determine whether the seller has made profit or
+    // // incurred loss or no profit no loss. Also determine
+    // // how much profit he made or loss he incurred.
+    
+    // float sp, cp;
+    // cout<< "Enter selling price (in rupees): ";
+    // cin>> sp;
+    // cout<< endl;
+    // cout<< "Enter cost price (in rupees): ";
+    // cin>> cp;
+    // cout<< endl;
+    // float x = sp-cp;
+    // if(x>0){
+    //     cout<< "total profit is : " << x << " rupees" << endl;
+    // }
+    // else if(x<0){
+    //     cout<< "total loss is : " << -x << " rupees" << endl;
+    // }
+    // else{
+    //     cout<< "there is neither profit nor loss" << endl;
+    // }
+
+    
+
+    // ***NOTE***: WE CAN USE BOTH and AND &&, SIMILARLY or AND ||
+    
+
+    // // QUESTION 2: GREATEST OF THREE NUMBERS
+    // // USING NESTED IF-ELSE
+
+    // int x,y,z;
+    // cout<< "enter 1st number: " ;
+    // cin>> x;
+    // cout<< "enter 2nd number: " ;
+    // cin>> y;
+    // cout<< "enter 3rd number: " ;
+    // cin>> z;
+    // if (x>y){
+    //     if(x>z){
+    //         cout<< x << " is the greatest number." << endl;
+    //     }
+    //     else{
+    //         cout<< z << " is the greatest number." << endl;
+    //     }
+    // }
+    // else{
+    //     if(y>z){
+    //         cout<< y << " is the greatest number." << endl;
+    //     }
+    //     else{
+    //         cout<< z << " is the greatest number." << endl;
+    //     }
+    // }
 
 
+    // // QUESTION 3: LEAST OF THREE NUMBERS
+    // // USING IF-ELSE
+    
+    // int x,y,z;
+    // cout<< "enter 1st number: " ;
+    // cin>> x;
+    // cout<< "enter 2nd number: " ;
+    // cin>> y;
+    // cout<< "enter 3rd number: " ;
+    // cin>> z;
+
+    // if((x<y) and (x<z)){
+    //     cout<< x << " is the least of them" << endl;
+    // }
+    // else if((y<x) && (y<z)){
+    //     cout<< y << " is the least of them" << endl;
+    // }
+    // else{
+    //     cout<< z << " is the least of them" << endl;
+    // }
+
+
+
+    // // QUESTION 4: GRAPH QUESTION
+    // // HW : Given a point (x, y), write a program to find
+    // // out if it lies in the 1st Quadrant, 2nd Quadrant, 3rd
+    // // Quadrant, 4th Quadrant, on the x-axis, y-axis or at
+    // // the origin, viz. (0, 0).
+
+    // float x, y;
+    // cout<< "Enter x coordinate: ";
+    // cin>> x;
+    // cout<< "Enter y coordinate: ";
+    // cin>> y;
+
+    // if (x==0 and y==0){
+    //     cout<< "at the origin";
+    // }
+    // else if (x==0 and y!=0){
+    //     cout<< "on y-axis";
+    // }
+    // else if (x=!0 and y==0){
+    //     cout<< "on x-axis";
+    // }
+    // else{
+    //     if(x>0){
+    //         if(y>0){
+    //             cout<< "1st quadrant";
+    //         }
+    //         else{
+    //             cout<< "4th quadrant";
+    //         }
+    //     }
+    //     else{
+    //         if(y>0){
+    //             cout<< "2nd quadrant";
+    //         }
+    //         else{
+    //             cout<< "3rd quadrant";
+    //         }
+    //     }
+    // }
 }
